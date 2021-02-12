7d6d13bf11106b889fea60523a8c00b28b172883
Niklas Tiede <niklastiede2@gmail.com>
2021-02-12 18:12:40 +0100

    sending emails

---
+++ requesting_apis/README.md
@@ -0,0 +1,10 @@
+# Requests
+
+Here I have created a powerful [requests](https://github.com/psf/requests) script.
+
+- dataclasses
+- marshmellow
+- pydantic (static typing)
+- desert
+
+![]()
---
+++ requesting_apis/testo.py
@@ -0,0 +1,79 @@
+
+import json
+from dataclasses import dataclass
+import dataclasses
+
+import requests
+from marshmallow import EXCLUDE, fields, pre_dump, Schema, validate
+import desert
+
+
+@dataclass
+class Activity:
+    activity: str
+    participants: int = dataclasses.field(metadata=desert.metadata(
+        fields.Int(
+            requird=True,
+            validate=validate.Range(
+                min=1, max=50, error='Participants must be between 1 and 50')
+        )
+    ))
+    price: float = dataclasses.field(metadata=desert.metadata(
+        fields.Float(
+            required=True,
+            validate=validate.Range(
+                min=0, max=.5, error="Price must be between 0 (0$) and .5 (50$)")
+        ))
+    )
+
+    def __post_init__(self):
+        """
+
+        """
+        self.price = self.price * 100
+
+
+def get_activity():
+    resp = requests.get('https://boredapi.com/api/activity').json()
+    schema = desert.schema(Activity, meta={"unknown": EXCLUDE})
+    return schema.load(resp)
+
+
+print(get_activity())
+
+
+# @dataclass
+# class Activity:
+#     activity: str
+#     participants: int
+#     price: float
+
+
+# class ActivitySchema(Schema):
+#     activity = fields.Str(required=True)
+#     participants = fields.Int(required=True)
+#     price = fields.Float(required=True)
+
+
+# def get_activity():
+#     resp = requests.get('https://boredapi.com/api/activity').json()
+#     validated_resp = ActivitySchema(unknown=EXCLUDE).load(resp)
+#     return Activity(
+#         activity=resp["activity"],
+#         participants=resp["participants"],
+#         price=resp["price"]
+#     )
+
+
+# print(get_activity())
+
+# with open('boredapi_request.json', 'w') as f:
+#    the_dump = json.dumps(activity, sort_keys=True, indent=4)
+#    f.write(the_dump)
+
+# --------------
+
+# dataclass makes more obvious which art of data the API returns
+# marshmellow is used to deserialize and **validate data**
+
+# easy way: for data validation, I should prfer to use something different than desert!
