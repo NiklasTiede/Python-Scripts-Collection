c1d5e52c7d7a88f34fc8e8054dd85915791846ad
Niklas Tiede <niklastiede2@gmail.com>
2021-02-13 02:30:58 +0100

    requesting apis

---
+++ requesting_apis/github_user/github_api.py
@@ -0,0 +1,24 @@
+
+import requests
+import configparser
+import json
+from pprint import pprint
+# credentials of sender email account:
+config = configparser.ConfigParser()
+config.read("../settings.ini")
+GITHUB_USERNAME = config.get("settings", "GITHUB_USERNAME")
+GITHUB_PASSWORD = config.get("settings", "GITHUB_PASSWORD")
+
+
+r = requests.get('https://api.github.com/user', auth=(GITHUB_USERNAME, GITHUB_PASSWORD))
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+print(r.text)
+print(r.json())
+
+pprint(r.json())
+
+with open('github_acc.json', 'w') as f:
+    json.dump(r.json(), f, sort_keys=True, indent=2)
+
